6577a494519f42c630f193665639c78a2f8c66b5
Shafaat <ershafaat@gmail.com>
2018-01-22 09:17:29 +0530

    Initial Commit

---
+++ src/test/java/webdriverProject/LoginTest.java
@@ -0,0 +1,37 @@
+package webdriverProject;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	public static WebDriver driver;
+	@BeforeTest
+	public void setup()
+	{
+		System.setProperty("webdriver.gecko.driver","E://GitHub//webdriverProject//geckodriver.exe");
+		driver=new FirefoxDriver();
+		driver.get("http://gmail.com//");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10L,TimeUnit.SECONDS);
+	}
+	@Test
+	public void doLogin() throws InterruptedException{
+		driver.findElement(By.id("identifierId")).sendKeys("ershafaat@gmail.com");
+		driver.findElement(By.id("identifierNext")).click();
+		Thread.sleep(4000);
+		driver.findElement(By.id("password")).sendKeys("shafi_FORu65");
+	
+	}
+	@AfterTest
+	public void teardown()
+	{
+		driver.quit();
+	}
+
+}
